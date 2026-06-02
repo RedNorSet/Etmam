90ebd89037a5ceb52b202fe95a31dd66f4f7f4b2
RedNorSet <stampy500@gmail.com>
2026-06-02 05:02:43 +0300

    Auto-create superuser on first deploy

--- build.sh
+++ build.sh
@@ -3,3 +3,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from accounts.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@etmam.com', 'Admin@11')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
